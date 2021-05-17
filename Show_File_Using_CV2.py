218479948b7758d1fbc24c06b4a9fefd4c0a25c0
Hamora Hadi <61151507+samyon7@users.noreply.github.com>
2021-05-17 20:17:17 +0700

    Sourcing Code
    Create the source code to show image without saving it

---
+++ Show_File_Using_CV2.py
@@ -0,0 +1,6 @@
+import cv2
+import matplotlib.pyplot as plt
+
+image = cv2.imread('../input/tomatoleaf/tomato/val/Tomato___Tomato_mosaic_virus/000ec6ea-9063-4c33-8abe-d58ca8a88878___PSU_CG 2169.JPG')
+plt.imshow(image)
+plt.show
